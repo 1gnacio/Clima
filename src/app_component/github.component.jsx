d9b004793ed50ef690290e29122f35d7c7ee73a1
Nacho <ignacio5garcia@hotmail.com>
2021-01-17 21:04:01 -0300

    bugfix

--- src/app_component/github.component.jsx
+++ src/app_component/github.component.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Github = () => {
-    return (
-        <div className="container">
-            <img src="assets/git.jpg" onClick={()=> window.open("https://github.com/1gnacio/Clima", "_blank")}/>
-        </div>
-    );
+class Github extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+    }
+    
+    clickHandler = () => {
+        window.open("https://github.com/1gnacio/Clima", "_blank")
+    }
+
+    mouseOverHandler = () => {
+        setCursor(prevState => {
+          if(prevState === 'crosshair'){
+            return 'pointer';
+          }
+          return 'crosshair';
+        })
+    };
+
+    render() {
+        return(
+            <div className="container">
+                <img src="../assets/git.jpg" onClick={this.clickHandler} onMouseOver={this.mouseOverHandler}/>
+            </div>
+        );
+    }
 }
 
 export default Github
