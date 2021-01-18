0a4db0f79ac3c55c4452fef52fbdaedf4e6c1e5f
Nacho <ignacio5garcia@hotmail.com>
2021-01-17 21:20:22 -0300

    bugfix2

--- src/app_component/github.component.jsx
+++ src/app_component/github.component.jsx
@@ -3,24 +3,25 @@ import React, { Component } from 'react'
 function Github() {
 
     const [cursor, setCursor] = useState('crosshair');
-    
-    clickHandler = () => {
-        window.open("https://github.com/1gnacio/Clima", "_blank")
+
+    const changeCursor = () => {
+        setCursor(prevState => {
+        if(prevState === 'crosshair'){
+            return 'pointer';
+        }
+        return 'crosshair';
+        });
     }
 
-    mouseOverHandler = (setCursor) => {
-        setCursor(prevState => {
-          if(prevState === 'crosshair'){
-            return 'pointer';
-          }
-          return 'crosshair';
-        })
-    };
+    const clickHandler = () => {
+        window.open("https://github.com/1gnacio/Clima", "_blank")
+    }
+    
     return(
         <div className="container">
             <img src="../assets/git.jpg" 
-            onClick={this.clickHandler} 
-            onMouseOver={this.mouseOverHandler(setCursor)} 
+            onClick={clickHandler} 
+            onMouseOver={changeCursor} 
             style={{cursor: cursor}}/>
         </div>
     );
